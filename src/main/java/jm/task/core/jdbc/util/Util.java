e8e170f1e35803dda4b97e9936010ad817e700a2
R1DZERO <omat.nasukhov@gmail.com>
2024-07-12 16:58:49 +0300

    Trying to solve a problem with many calls method getConnection().

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,15 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
-   private static final String URL = "jdbc:mysql://localhost:3306/usersDB";
-   private static final String USERNAME = "root";
-   private static final String PASSWORD = "Mamku228";
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
+    private static final String URL = "jdbc:mysql://localhost:3306/usersDB";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Mamku228";
+    private static Connection connection = null;
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
+        return connection;
     }
 }
